761ef52eb7a440f86bc36351f2456f3e9826eae6
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2021-08-23 17:35:49 -0300

    small updates

--- fix-acceptor-kafka/src/main/java/com/redhat/lot/poc/fixacceptor/FixMessageProcessor.java
+++ fix-acceptor-kafka/src/main/java/com/redhat/lot/poc/fixacceptor/FixMessageProcessor.java
@@ -1,38 +1,26 @@
 package com.redhat.lot.poc.fixacceptor;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
-import quickfix.ConfigError;
 import quickfix.DoubleField;
 import quickfix.FieldNotFound;
 import quickfix.InvalidMessage;
 import quickfix.Message;
-import quickfix.MessageUtils;
-import quickfix.RuntimeError;
-import quickfix.Session;
-import quickfix.SessionSettings;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.logging.Logger;
 
 @ApplicationScoped
 public class FixMessageProcessor {
 
     private Logger LOGGER = Logger.getLogger(FixMessageProcessor.class.getName());
-    
+
     @Incoming("marketdata")
     public void process(String msg) throws InterruptedException, FieldNotFound, InvalidMessage {
-        // LOGGER.info("Fix Message received: "+msg.toString());
         Message fixMessage = new Message();
         fixMessage.fromString(msg, null, false);
-        LOGGER.info(">>>>>>> "+fixMessage.getField(new DoubleField(60)).toString());
+        LOGGER.info("Fix Message received field (60): "+fixMessage.getField(new DoubleField(60)).toString());
     }
 
 }
