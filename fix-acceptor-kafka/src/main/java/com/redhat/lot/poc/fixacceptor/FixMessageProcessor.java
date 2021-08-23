b4c4ac787bdf4c1bf91b77e4f0430375ddeaf60d
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2021-08-23 16:32:24 -0300

    Improvements trying to deserialize quickfix Message

--- fix-acceptor-kafka/src/main/java/com/redhat/lot/poc/fixacceptor/FixMessageProcessor.java
+++ fix-acceptor-kafka/src/main/java/com/redhat/lot/poc/fixacceptor/FixMessageProcessor.java
@@ -1,21 +1,71 @@
 package com.redhat.lot.poc.fixacceptor;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
+import quickfix.ConfigError;
+import quickfix.InvalidMessage;
+import quickfix.Message;
+import quickfix.MessageUtils;
+import quickfix.RuntimeError;
+import quickfix.Session;
+import quickfix.SessionSettings;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Logger;
 
 @ApplicationScoped
 public class FixMessageProcessor {
 
     private Logger LOGGER = Logger.getLogger(FixMessageProcessor.class.getName());
+    private Executor executor;
 
+    @Produces
+    public Executor executor() throws Exception {
+        InputStream inputStream = getSettingsInputStream();
+        SessionSettings settings = new SessionSettings(inputStream);
+       
+        Executor executor = new Executor(settings);
+        return executor;
+    }
+
+    private InputStream getSettingsInputStream(){
+		InputStream inputStream = Executor.class.getResourceAsStream("executor.cfg");
+		if (inputStream == null) {
+			System.out.println("usage: " + Executor.class.getName() + " [configFile].");
+			System.exit(1);
+		} else {
+			System.out.println("Arquivo de conf de FIX encontrado!");
+		}
+		return inputStream;
+	}
+
+    @PostConstruct 
+    void init() throws Exception {
+        InputStream inputStream = getSettingsInputStream();
+        SessionSettings settings = new SessionSettings(inputStream);
+       
+        executor = new Executor(settings);
+        executor.start();
+    }
 
     @Incoming("marketdata")
-    public void process(String message) throws InterruptedException {
+    public void process(String msg) throws InterruptedException, InvalidMessage {
+        LOGGER.info("Fix Message received: "+msg.toString());
+        // Message message = MessageUtils.parse(this.executor.getApplication().getFixSession(), msg);
+        // if (message != null){
+        //     System.out.println("Message succesfully received");
+        // }
+    }
 
-        LOGGER.info("Fix Message received: "+message.toString());
-
+    @PreDestroy
+    public void destroy(){
+        executor.stop();
     }
 }
---
+++ fix-acceptor-kafka/src/main/java/com/redhat/lot/poc/fixacceptor/FixSessionMain.java
@@ -0,0 +1,38 @@
+// package com.redhat.lot.poc.fixacceptor;
+
+// import java.io.FileNotFoundException;
+// import java.io.InputStream;
+
+// import io.quarkus.runtime.Quarkus;
+// import io.quarkus.runtime.annotations.QuarkusMain;
+// import quickfix.SessionSettings;
+
+// @QuarkusMain
+// public class FixSessionMain {
+
+// 	public static void main(String... args) throws Exception {
+// 		try (InputStream inputStream = getSettingsInputStream(args)) {
+// 			SessionSettings settings = new SessionSettings(inputStream);
+// 			inputStream.close();
+
+// 			Executor executor = new Executor(settings);
+// 			executor.start();
+
+// 			Quarkus.run(args); 
+//             Quarkus.waitForExit();
+
+// 			executor.stop();
+// 		}
+// 	}
+
+// 	private static InputStream getSettingsInputStream(String[] args) throws FileNotFoundException {
+// 		InputStream inputStream = Executor.class.getResourceAsStream("executor.cfg");
+// 		if (inputStream == null) {
+// 			System.out.println("usage: " + Executor.class.getName() + " [configFile].");
+// 			System.exit(1);
+// 		} else {
+// 			System.out.println("Arquivo de conf de FIX encontrado!");
+// 		}
+// 		return inputStream;
+// 	}
+// }
